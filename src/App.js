ea265ebf637d2d56f735caeed35d158c040c8b5d
Karabo Sambo <karabosambo@outlook.com>
2021-10-27 16:38:22 +0200

    weather done. mostly

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import "./sass/main.scss";
 import Sidebar from "./components/Sidebar";
 import Tasks from "./pages/Tasks";
+import Dashboard from "./pages/Dashboard";
+import {Switch, Route,Redirect} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <main className="App__main">
-        <Tasks />
+        <Switch>
+            <Route exact path="/"><Redirect to="/dashboard"/></Route> 
+            <Route path="/dashboard"><Dashboard/></Route>
+            <Route path="/tasks"><Tasks /></Route>
+        </Switch>
       </main>
     </div>
   );
